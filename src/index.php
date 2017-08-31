67d8cef1f8d67fb377b10f17023db295d10ce671
Lung <lung@skaut.cz>
2017-08-31 22:01:40 +0200

    added menu, prefix+suffix, styling, view for log and lending mechanism

--- src/index.php
+++ src/index.php
@@ -37,6 +37,15 @@ $container['db'] = function ($c) {
 $container['view'] = new \Slim\Views\PhpRenderer("../templates/");
 
 
+// MIDDLEWARE
+
+$app->add(function (Request $request, Response $response, $next) {
+	$response = $next($request, $response);
+	
+	return $response;
+});
+
+
 // ROUTES
 
 $app->get('/phpinfo', function (Request $request, Response $response) {
@@ -44,9 +53,7 @@ $app->get('/phpinfo', function (Request $request, Response $response) {
 });
 
 $app->get('/new-radio', function (Request $request, Response $response) {
-	$response = $this->view->render($response, 'new-radio.phtml', ['router' => $this->router]);
-	
-	return $response;
+	return $response = $this->view->render($response, 'new-radio.phtml', ['router' => $this->router]);
 })->setName('new-radio');
 
 $app->post('/add-new-radio', function (Request $request, Response $response) {
@@ -57,29 +64,75 @@ $app->post('/add-new-radio', function (Request $request, Response $response) {
 			htmlspecialchars($parsedBody['radioId'], ENT_QUOTES),
 			htmlspecialchars($parsedBody['name'], ENT_QUOTES),
 			'ready',
-			'NOW()',
+			date('Y-m-d H:i:s'),
 			NULL,
 		]
 	);
+	$this->logger->addInfo('Added radio with ID '.htmlspecialchars($parsedBody['radioId'], ENT_QUOTES));
 	
 	return $response->withHeader('Location', $this->router->pathFor('radio-list'));
-})->
-setName('add-new-radio');
+})->setName('add-new-radio');
+
+$app->post('/lend-radio', function (Request $request, Response $response) {
+	$parsedBody = $request->getParsedBody();
+	$id = htmlspecialchars($parsedBody['id'], ENT_QUOTES);
+	$radioId = htmlspecialchars($parsedBody['radioId'], ENT_QUOTES);
+	
+	$query = $this->db->prepare('UPDATE `radios` SET `status` = ? WHERE `id` = ?');
+	$query->execute(['lent', $id]);
+	
+	$this->logger->addInfo('Lent radio with ID '.$radioId);
+	
+	return $response->withHeader('Location', $this->router->pathFor('radio-list'));
+})->setName('lend-radio');
+
+$app->post('/return-radio', function (Request $request, Response $response) {
+	$parsedBody = $request->getParsedBody();
+	$id = htmlspecialchars($parsedBody['id'], ENT_QUOTES);
+	$radioId = htmlspecialchars($parsedBody['radioId'], ENT_QUOTES);
+	
+	$query = $this->db->prepare('UPDATE `radios` SET `status` = ? WHERE `id` = ?');
+	$query->execute(['returned', $id]);
+	
+	$this->logger->addInfo('Returned radio with ID '.$radioId);
+	
+	return $response->withHeader('Location', $this->router->pathFor('radio-list'));
+})->setName('return-radio');
+
+$app->get('/log', function (Request $request, Response $response) {
+	$logData = file_get_contents('../logs/rtls.log');
+	
+	return $response = $this->view->render($response, 'log.phtml', ['router' => $this->router, 'log' => explode(PHP_EOL, $logData)]);
+})->setName('log');
 
 $app->get('/', function (Request $request, Response $response) {
-	$this->logger->addInfo('Main page accessed.');
-	
 	//get items from DB
-	$query = $this->db->query('SELECT `radioId`, `name`, `status`, `last-returned-time`, `last-borrower` FROM `radios`');
+	$query = $this->db->query('SELECT `id`,`radioId`, `name`, `status`, `last-returned-time`, `last-borrower` FROM `radios`');
 	$radios = $query->fetchAll();
+	//get right link based by status
+	foreach ($radios as &$r) {
+		switch ($r['status']) {
+			case 'ready':
+				//lend available
+				$r['link'] = $this->router->pathFor('lend-radio');
+				$r['linkLabel'] = 'Vypůjčit';
+				break;
+			case 'lent':
+				//return available
+				$r['link'] = $this->router->pathFor('return-radio');
+				$r['linkLabel'] = 'Vrátit';
+				break;
+			case 'returned':
+				//lend available (but with exceptions?)
+				$r['link'] = $this->router->pathFor('lend-radio');
+				$r['linkLabel'] = 'Vypůjčit (nenabito!)';
+				break;
+		}
+	}
 	
-	//render main page
-	$response = $this->view->render($response, 'radio-list.phtml', ['router' => $this->router, 'radios' => $radios]);
-	return $response;
+	return $response = $this->view->render($response, 'radio-list.phtml', ['router' => $this->router, 'radios' => $radios]);
 })->setName('radio-list');
 
 
 // FIRE!
-
-$container['logger']->addInfo("RTLS starting.");
 $app->run();
