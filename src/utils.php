8103917f2f565f7fb8bd9b5e8a44b24697c63c86
Lung <lung@skaut.cz>
2023-05-02 12:09:23 +0200

    added filtering without diacritic

--- src/utils.php
+++ src/utils.php
@@ -3,3 +3,34 @@
 function getNow() {
     return (new DateTime('now', new DateTimeZone('Europe/Prague')))->format('Y-m-d H:i:s');
 }
+
+function removeDiacritic($input) {
+    $diacritic = [
+        '/[áàâãªä]/u' => 'a',
+        '/[ÁÀÂÃÄ]/u' => 'A',
+        '/[čç]/u' => 'c',
+        '/[ČÇ]/u' => 'C',
+        '/[ď]/u' => 'd',
+        '/[Ď]/u' => 'D',
+        '/[éèêëéě]/u' => 'e',
+        '/[ÉÈÊËÉĚ]/u' => 'E',
+        '/[íìîï]/u' => 'i',
+        '/[ÍÌÎÏ]/u' => 'I',
+        '/[ňñ]/u' => 'n',
+        '/[ŇÑ]/u' => 'N',
+        '/[óòôõºö]/u' => 'o',
+        '/[ÓÒÔÕÖ]/u' => 'O',
+        '/[ř]/u' => 'r',
+        '/[Ř]/u' => 'R',
+        '/[š]/u' => 's',
+        '/[Š]/u' => 'S',
+        '/[ť]/u' => 't',
+        '/[Ť]/u' => 'T',
+        '/[úùûüúů]/u' => 'u',
+        '/[ÚÙÛÜÚŮ]/u' => 'U',
+        '/[ž]/u' => 'z',
+        '/[Ž]/u' => 'Z',
+    ];
+
+    return preg_replace(array_keys($diacritic), array_values($diacritic), $input);
+}
